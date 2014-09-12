45afa1fa1e9b0d28cb540014c369c1fe7ac9896e
AnwarMohamed <anwarelmakrahy@gmail.com>
2014-09-12 03:54:10 +0300

    initial client

---
+++ client/app/src/main/java/com/anwarelmakrahy/xmppclient/MainActivity.java
@@ -0,0 +1,52 @@
+package com.anwarelmakrahy.xmppclient;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        new Thread() {
+            @Override
+            public void run() {
+                XMPPClient client = new XMPPClient("jabber.alpha-labs.net", 5222);
+
+                try {
+                    client.init();
+                    client.authenticate("anwarelmakrahy", "123456");
+                    client.createEntry("anwarelmakrahy", "Anwar Mohamed");
+                    client.getServers();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }.start();
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
